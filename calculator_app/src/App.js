4ecf8e9a1f12a1fc623b3f7b7387823e468b0474
Einstein <einsteinnnamah@gmail.com>
2022-10-11 22:40:03 +0100

    new files

---
+++ calculator_app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <div className="help text-blue-900">Help</div>
+    </div>
+  );
+}
+
+export default App;
